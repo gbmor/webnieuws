991bf23b847245b4182d56182db2f8f3010ae52a
Ben Morrison <ben@gbmor.dev>
2019-09-03 01:30:26 -0400

    listening on domain sock

--- service/src/main.rs
+++ service/src/main.rs
@@ -6,22 +6,42 @@
 use ctrlc;
 use log;
 
-use std::os::unix::net::{UnixListener, UnixStream};
+use std::os::unix::net::UnixListener;
 use std::thread;
 
 mod db;
 mod logging;
 
+const SOCK_PATH: &str = "/tmp/webnieuws.sock";
+
 fn main() {
     logging::init();
 
     //more will have to be done here later
-    ctrlc::set_handler(move || {
+    ctrlc::set_handler(|| {
         log::warn!("^C/SIGINT Caught ... ");
+
         std::process::exit(0);
-    });
+    })
+    .expect("Failed to set up SIGINT handler");
 
     log::info!("Starting up ...");
 
-    thread::spawn(db::Conn::open(db::PATH));
+    // This will loop, ensuring if the thread dies it's
+    // immediately respawned.
+    loop {
+        thread::spawn(|| {
+            let _db = db::Conn::open(db::PATH);
+            log::info!("Database connection opened: {}", db::PATH);
+
+            let lstnr = UnixListener::bind(SOCK_PATH).unwrap();
+            log::info!("Listening on socket: {}", SOCK_PATH);
+            for strm in lstnr.incoming() {
+                match strm {
+                    Ok(_stream) => {}
+                    Err(err) => log::error!("{:?}", err),
+                }
+            }
+        });
+    }
 }
